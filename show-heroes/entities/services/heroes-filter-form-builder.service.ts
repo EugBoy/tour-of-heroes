bd21b0e1a0b921c0cc519f9b1e928cdd46756e31
EugBoy <68354581+EugBoy@users.noreply.github.com>
2023-10-01 22:15:42 +0300

    Add files via upload

---
+++ show-heroes/entities/services/heroes-filter-form-builder.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesFilterFormBuilderService {
+
+  constructor (
+    private readonly _formBuilder: FormBuilder
+  ) {
+  }
+
+  /**
+   * Метод создания формы для фильтрации
+   */
+  public get filterForm(): FormGroup{
+    return this._formBuilder.group({
+      levelDown: ['', [Validators.pattern(/^-?(0|[1-9]\d*)?$/)]],
+      levelUp: ['',Validators.pattern(/^-?(0|[1-9]\d*)?$/)],
+      skills: [''],
+      name: [''],
+    })
+  }
+}
